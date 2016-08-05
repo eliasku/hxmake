965a7ba6c08d664de633a289f75705b9cd9f84a6
eliasku <deadbabe@gmail.com>
2016-08-05 16:38:31 +0300

    missed class, plugin inclusion

---
+++ src/hxmake/macr/PluginInclude.hx
@@ -0,0 +1,50 @@
+package hxmake.macr;
+
+import haxe.macro.Compiler;
+import hxmake.utils.Haxelib;
+import haxe.Json;
+import sys.io.File;
+import sys.FileSystem;
+import haxe.io.Path;
+
+class PluginInclude {
+    static var _makeLibraries:Array<String> = [];
+
+    public static function scan(path:String) {
+        var pluginsPath = Path.join([path, "plugins.json"]);
+        if(FileSystem.exists(pluginsPath)) {
+            try {
+                var content = File.getContent(pluginsPath);
+                var data = Json.parse(content);
+                for(field in Reflect.fields(data)) {
+                    include(field, Reflect.field(data, field));
+                }
+            }
+            catch(e:Dynamic) {}
+        }
+    }
+
+    public static function include(name:String, path:String):Bool {
+        if(_makeLibraries.indexOf(name) >= 0) {
+            return true;
+        }
+
+        // TODO: relative path, git, accurate haxelib
+        var lp = Haxelib.libPath(name, true);
+        if(lp == null) {
+            Haxelib.install(name);
+            lp = Haxelib.libPath(name, true);
+        }
+        if(lp == null) {
+            return false;
+        }
+        var cp = Path.join([lp, "makeplugin"]);
+        if(FileSystem.exists(cp)) {
+            Sys.println('Make Plugin: $name @ $cp');
+            Compiler.addClassPath(cp);
+        }
+
+        _makeLibraries.push(name);
+        return true;
+    }
+}
