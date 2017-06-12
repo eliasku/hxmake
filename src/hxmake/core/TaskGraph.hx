c6fc5807199550ad1b696216f6222fd8057a001f
Roman Mikhailov <roman.mikhailov.sp@gmail.com>
2017-06-12 13:16:19 +0300

    Change task resolving.
    1. Add detecting of cycling task dependencies.
    2. Add detecting of missed tasks.
    3. Fix incorrect tasks resolution (before, after, depended)

--- src/hxmake/core/TaskGraph.hx
+++ src/hxmake/core/TaskGraph.hx
@@ -1,39 +1,50 @@
 package hxmake.core;
 
 import hxmake.cli.MakeLog;
+import hxmake.Task;
 import hxmake.cli.CL;
 
 @:final
 @:access(hxmake.Module)
 @:access(hxmake.Task)
 class TaskGraph {
-
-	var _nodes:Array<TaskNode> = [];
 	var _modules:Array<Module>;
 	var _runTasks:Array<String>;
-	var _orderData:Map<String, String> = new Map();
+	var _taskNodes:Array<TaskNode> = [];
 
-	function new(runTasks:Array<String>, modules:Array<Module>) {
+	function new(args:Array<String>, modules:Array<Module>) {
 		_modules = modules;
-		_runTasks = runTasks;
+		_runTasks = getTasksNameFromArgs(args);
+	}
+
+	static function getTasksNameFromArgs(args:Array<String>):Array<String> {
+		var result:Array<String> = [];
+		for (arg in args) {
+			if (arg.charAt(0) != "-") {
+				result.push(arg);
+			}
+		}
+		return result;
 	}
 
 	public function build() {
-		_runTasks = resolveDependencies(_runTasks, _modules);
+		var initialTasks:Array<TaskNode> = findInitialTasks(_runTasks, _modules);
+		var allTasksToRunUnordered:Array<TaskNode> = findDependedTasks(initialTasks, _modules);
+		var allTasksToRunOrdered:Array<TaskNode> = resolveTasksOrder(allTasksToRunUnordered);
+		_taskNodes = allTasksToRunOrdered;
 		configureTasks();
-		resolveOrder();
 	}
 
 	public function printTasks() {
 		MakeLog.info("Task dependency order: ");
-		for (node in _nodes) {
-			MakeLog.info("\t" + node.task.module.name + "." + node.name);
+		for (taskNode in _taskNodes) {
+			MakeLog.info("\t" + taskNode.module.name + "." + taskNode.name);
 		}
 	}
 
 	public function run() {
-		for (node in _nodes) {
-			var task = node.task;
+		for (taskNode in _taskNodes) {
+			var task = taskNode.task;
 			if (task.enabled) {
 				CL.workingDir.push(task.module.path);
 				@:privateAccess task._run();
@@ -42,67 +53,174 @@ class TaskGraph {
 		}
 	}
 
-	static function resolveDependencies(requiredTasks:Array<String>, modules:Array<Module>):Array<String> {
-		var runTasks = requiredTasks.copy();
-		var process:Bool = true;
-		while (process) {
-			process = false;
+	static function findInitialTasks(tasksNames:Array<String>, modules:Array<Module>):Array<TaskNode> {
+		var result:Array<TaskNode> = [];
+		var atLeastOneFound:Bool = false;
+		for (taskName in tasksNames) {
+			atLeastOneFound = false;
 			for (module in modules) {
-				var tasks = module._tasks;
-				for (taskId in tasks.keys()) {
-					var ct:Task = tasks.get(taskId);
-					if (runTasks.indexOf(taskId) >= 0) {
-						for (depTask in ct.__depends) {
-							if (runTasks.indexOf(depTask) < 0) {
-								process = true;
-								runTasks.push(depTask);
-							}
-						}
+				var task:Task = module.getTask(taskName);
+				if (task != null) {
+					result.push(new TaskNode(taskName, task));
+					atLeastOneFound = true;
+				}
+			}
+			if (!atLeastOneFound) {
+				throw 'Task with name `${taskName}` not found.';
+			}
+		}
+		return result;
+	}
+
+	static function findDependedTasks(initialTasks:Array<TaskNode>, modules:Array<Module>):Array<TaskNode> {
+		var tasks:Array<TaskNode> = initialTasks.copy();
+		for (task in tasks) {
+			findDependedTask(task, tasks);
+		}
+		return tasks;
+	}
+
+	static function findDependedTask(taskNode:TaskNode, addedTasks:Array<TaskNode>):Void {
+		var currentTask:Task = taskNode.task;
+		var taskModule:Module = taskNode.module;
+		for (depended in currentTask.__depends.iterator()) {
+			var alreadyExists:Bool = false;
+			for (addedTask in addedTasks) {
+				if (addedTask.name == depended && addedTask.module == taskModule) {
+					alreadyExists = true;
+					break;
+				}
+			}
+			if (!alreadyExists) {
+				var newTask:Task = taskModule.getTask(depended);
+				if (newTask == null) {
+					throw 'Task with name `${depended}` not found in module `${taskModule.name}`. Specified as dependency to `${taskNode.name}`';
+				}
+				var newTaskDef:TaskNode = new TaskNode(depended, newTask);
+				addedTasks.push(newTaskDef);
+				findDependedTask(newTaskDef, addedTasks);
+			}
+		}
+	}
+
+	static function resolveTasksOrder(tasks:Array<TaskNode>):Array<TaskNode> {
+		tasks = tasks.copy();
+		var tasksLength:Int = tasks.length;
+
+		var allTasksIndexes:Map<TaskNode, Int> = new Map<TaskNode, Int>();
+		for (i in 0...tasksLength) {
+			allTasksIndexes.set(tasks[i], i);
+		}
+
+		var executionOrderRelations:Array<Array<Int>> = new Array<Array<Int>>();
+		for (i in 0...tasksLength) {
+			executionOrderRelations.push(new Array<Int>());
+			for (j in 0...tasksLength) {
+				executionOrderRelations[i][j] = 0;
+			}
+		}
+
+		// Manadatory execution list flow order
+		for (task in tasks) {
+			var taskIndex:Int = allTasksIndexes.get(task);
+			for (depended in task.task.__depends) {
+				var dependedTask:TaskNode = null;
+				for (it in tasks) {
+					if (it.name == depended && task.task.module == it.task.module) {
+						dependedTask = it;
+						break;
 					}
 				}
+				if (dependedTask == null) {
+					throw 'Illegal state. Task `${depended}` added as dependency to task `${task.name} but received null task value.`';
+				}
+				var relationIndex:Int = allTasksIndexes.get(dependedTask);
+				executionOrderRelations[taskIndex][relationIndex] = 1;
 			}
 		}
-		return runTasks;
-	}
 
-	function configureTasks() {
-		for (module in _modules) {
-			var tasks = module._tasks;
-			CL.workingDir.push(module.path);
-			for (taskId in tasks.keys()) {
-				if (_runTasks.indexOf(taskId) >= 0) {
-					var task:Task = tasks.get(taskId);
-					task._configure();
-					_nodes.push(new TaskNode(taskId, task));
+		// Optional execution list flow order (after)
+		for (task in tasks) {
+			var taskIndex:Int = allTasksIndexes.get(task);
+			for (runAfter in task.task.__after) {
+				var runAfterTask:TaskNode = null;
+				for (it in tasks) {
+					if (it.name == runAfter && task.task.module == it.task.module) {
+						runAfterTask = it;
+						break;
+					}
+				}
+				if (runAfterTask == null) {
+					continue;
+				}
+				var relationIndex:Int = allTasksIndexes.get(runAfterTask);
+				executionOrderRelations[taskIndex][relationIndex] = 1;
+			}
+		}
+
+		// Optional execution list flow order (before)
+		for (task in tasks) {
+			var taskIndex:Int = allTasksIndexes.get(task);
+			for (runBefore in task.task.__before) {
+				var runBeforeTask:TaskNode = null;
+				for (it in tasks) {
+					if (it.name == runBefore && task.task.module == it.task.module) {
+						runBeforeTask = it;
+						break;
+					}
+				}
+				if (runBeforeTask == null) {
+					continue;
+				}
+				var relationIndex:Int = allTasksIndexes.get(runBeforeTask);
+				executionOrderRelations[relationIndex][taskIndex] = 1;
+			}
+		}
+
+		var executionOrder:Array<TaskNode> = [];
+		var usage:Array<Bool> = new Array<Bool>();
+		for (i in 0...tasksLength) usage.push(true);
+		for (i in 0...tasksLength) {
+			for (j in 0...tasksLength) {
+				if (!usage[j]) {
+					continue;
+				}
+				var containsOnlyZeros:Bool = true;
+				for (k in 0...tasksLength) {
+					if (usage[k] && executionOrderRelations[j][k] == 1) {
+						containsOnlyZeros = false;
+						break;
+					}
+				}
+				if (containsOnlyZeros) {
+					// Task without relations.
+					executionOrder.push(tasks[j]);
+					usage[j] = false;
 				}
 			}
+		}
+
+		if (usage.indexOf(true) != -1) {
+			var cycling:Array<TaskNode> = [];
+			for (i in 0...tasksLength) {
+				if (usage[i]) {
+					cycling.push(tasks[i]);
+				}
+			}
+			var cyclingWarn:String = cycling.map(function(taskDef:TaskNode):String {
+				return '${taskDef.task.module.name}.${taskDef.name}';
+			}).join(", ");
+			throw 'There are cycling dependencies or run order between next tasks: `${cyclingWarn}`';
+		}
+
+		return executionOrder;
+	}
+
+	function configureTasks():Void {
+		for (taskDefinition in _taskNodes) {
+			CL.workingDir.push(taskDefinition.task.module.path);
+			taskDefinition.task._configure();
 			CL.workingDir.pop();
 		}
-	}
-
-	function resolveOrder() {
-		for (node in _nodes) {
-			var task = node.task;
-			for (taskAfter in task.__before) {
-				_orderData.set(node.name, taskAfter);
-			}
-			for (taskBefore in task.__after) {
-				_orderData.set(taskBefore, node.name);
-			}
-		}
-
-		_nodes.sort(orderComparator);
-	}
-
-	function orderComparator(a:TaskNode, b:TaskNode) {
-		var after = _orderData.get(b.name);
-		var prevAfter:String;
-		while (after != null) {
-			if (a.name == after) {
-				return 1;
-			}
-			after = _orderData.get(after);
-		}
-		return -1;
 	}
 }
